616485f9e366f889d82ef9a7be7fd66141fb0de7
SupremeAmer <emmyplumbingservice@gmail.com>
2025-02-27 22:37:35 +0100

    Update and rename script.jsj to script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const projectName = document.querySelector('#project-name').value;
+  const projectDescription = document.querySelector('#project-description').value;
+  const clientName = document.querySelector('#client-name').value;
+  const clientEmail = document.querySelector('#client-email').value;
+  const paymentMethod = document.querySelector('#payment-method').value;
+  const budget = document.querySelector('#budget').value;
+
+  const emailBody = `
+    Project Name: ${projectName}
+    Project Description: ${projectDescription}
+    Client Name: ${clientName}
+    Client Email: ${clientEmail}
+    Payment Method: ${paymentMethod}
+    Budget: ${budget}
+  `;
+
+  const emailSubject = 'New Project Request';
+
+  window.location.href = `mailto:info@supremeamer.com?subject=${emailSubject}&body=${emailBody}`;
+});
